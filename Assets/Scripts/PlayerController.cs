3d060e66399348db32272ee2eaa2df30b7ac133f
Alexey Tsvetikov <xzxwarriarxzx@gmail.com>
2026-06-11 21:40:20 +0300

    PlayerController added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public InputAction moveAction;
+    public Vector2 moveInput;
+    public float speed = 10.0f;
+    public float xRange = 20.0f;
+
+    private void OnEnable()
     {
-        
+        moveAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -xRange)
+        { 
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        moveInput = moveAction.ReadValue<Vector2>();
+
+        transform.Translate(moveInput.x * speed * Time.deltaTime, 0, 0);
     }
 }
